8e87474fba1f70d1cf5fb618dfeaa1726ebd693d
Artur Pilipenko <apg-a@mail.ru>
2024-01-03 15:06:43 +0200

    Добавил описание задания в класс Main

--- src/HW_Lesson1/sample/Main.java
+++ src/HW_Lesson1/sample/Main.java
@@ -5,6 +5,11 @@ import HW_Lesson1.files_write.WriteFile;
 import java.io.IOException;
 import java.util.Scanner;
 
+/*
+Создать приложение с вложенностью пакетов не менее 3-х, где будет класс для входа и несколько классов с логикой.
+Пример: приложение для внесения заметок во внешний файл с обязательной фиксацией времени
+*/
+
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
